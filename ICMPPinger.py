74a7c10dcefe2cca5a0d9dba18c097655b9c118e
Jose Carlos Martinez Garcia-Vaso <carlosgvaso@gmail.com>
2019-03-25 19:31:57 -0500

    Cleaned out the data printed, and added socket error handling.

--- ICMPPinger.py
+++ ICMPPinger.py
@@ -19,8 +19,8 @@ ICMP_DEST_HOST_UNKNOWN_CODE = 7
 
 
 def calcStats(rttList):
-    rttMax = 0.0
-    rttMin = 1.0
+    rttMax = -1.0
+    rttMin = 2000.0
     rttSum = 0.0
     rttCount = 0
     lossCount = 0
@@ -36,25 +36,25 @@ def calcStats(rttList):
         else:
             lossCount += 1
 
-    lossRate = 100 * lossCount / (rttCount + lossCount)
+    if rttCount + lossCount > 0:
+        lossRate = 100 * lossCount / (rttCount + lossCount)
+    else:
+        lossRate = 100
+
     if rttCount > 0:
         rttAvg = rttSum / rttCount
+        print('\nMin/Max/Avg RTT: {0:.3f}/{1:.3f}/{2:.3f}, Packet Loss Rate: {3}%'
+              .format(rttMin, rttMax, rttAvg, lossRate))
     else:
-        rttAvg = 'N/A'
-    if rttMax == 0.0:
-        rttMax = 'N/A'
-    if rttMin == 1.0:
-        rttMin = 'N/A'
+        print('\nMin/Max/Avg RTT: na/na/na, Packet Loss Rate: {0}%'.format(lossRate))
 
-    print('\nAvg RTT: {0}, Max RTT: {1}, Min RTT: {2}, Packet Loss Rate: {3}%'
-          .format(rttAvg, rttMax, rttMin, lossRate))
 
 def checksum(string):
     csum = 0
     countTo = (len(string) // 2) * 2
     count = 0
     while count < countTo:
-        thisVal = ord(string[count+1]) * 256 + ord(string[count])
+        thisVal = ord(string[count + 1]) * 256 + ord(string[count])
         csum = csum + thisVal
         csum = csum & 0xffffffff
         count = count + 2
@@ -70,6 +70,7 @@ def checksum(string):
     answer = answer >> 8 | (answer << 8 & 0xff00)
     return answer
 
+
 def receiveOnePing(mySocket, ID, timeout, destAddr):
     timeLeft = timeout
 
@@ -77,8 +78,8 @@ def receiveOnePing(mySocket, ID, timeout, destAddr):
         startedSelect = time.time()
         whatReady = select.select([mySocket], [], [], timeLeft)
         howLongInSelect = (time.time() - startedSelect)
-        if whatReady[0] == []: # Timeout
-            return "Request timed out."
+        if whatReady[0] == []:  # Timeout
+            return "Request timed out.", 'na'
 
         timeReceived = time.time()
         recPacket, addr = mySocket.recvfrom(1024)
@@ -92,13 +93,20 @@ def receiveOnePing(mySocket, ID, timeout, destAddr):
         ipVersion, ipTypeOfSvc, ipLength, ipID, ipFlags, ipTTL, ipProtocol, ipChecksum, ipSrcIP, ipDestIP = \
             struct.unpack("!BBHHHBBHII", ipHeader)
         icmpType, icmpCode, icmpChecksum, packetID, packetSequence = struct.unpack('bbHHh', icmpHeader)
-        timeSent = struct.unpack('d', packetData)[0]
+
+        if len(packetData) == 8:
+            timeSent = struct.unpack('d', packetData)[0]
+        else:
+            timeSent = None
 
         expectedIcmpChecksum = 0
         # Make a dummy header with a 0 checksum
         # struct -- Interpret strings as packed binary data
         header_checksum = struct.pack("bbHHh", icmpType, icmpCode, expectedIcmpChecksum, packetID, packetSequence)
-        data_checksum = struct.pack("d", timeSent)
+        if len(packetData) == 8:
+            data_checksum = struct.pack("d", timeSent)
+        else:
+            data_checksum = bytes()
         # Calculate the checksum on the data and the dummy header.
         expectedIcmpChecksum = checksum(str(header_checksum + data_checksum))
 
@@ -110,45 +118,49 @@ def receiveOnePing(mySocket, ID, timeout, destAddr):
             expectedIcmpChecksum = htons(expectedIcmpChecksum)
 
         # Print all the values for debugging
-        #print('ipSrcIP:{0}, expected: {1}'.format(ipSrcIP, destAddr))
-        #print('ipTTL:{0}, expected: {1}'.format(ipTTL, '?'))
-        #print('addr:{0}, expected: {1}'.format(addr[0], destAddr))
-        #print('recPacket: {0}'.format(binascii.hexlify(recPacket)))
-        #print('icmpHeader: {0}'.format(binascii.hexlify(icmpHeader)))
-        #print('packetData: {0}'.format(binascii.hexlify(packetData)))
-        #print('icmpType: {0}, expected: {1}'.format(icmpType, ICMP_ECHO_REPLY_TYPE))
-        #print('icmpCode: {0}, expected: {1}'.format(icmpCode, ICMP_ECHO_REPLY_CODE))
-        #print('icmpChecksum: {0}, expected: {1}'.format(icmpChecksum, expectedIcmpChecksum))
-        #print('packetID: {0}, expected: {1}'.format(packetID, ID))
+        # print('ipSrcIP:{0}, expected: {1}'.format(ipSrcIP, destAddr))
+        # print('ipTTL:{0}, expected: {1}'.format(ipTTL, '?'))
+        # print('addr:{0}, expected: {1}'.format(addr[0], destAddr))
+        # print('recPacket: {0}'.format(binascii.hexlify(recPacket)))
+        # print('icmpHeader: {0}'.format(binascii.hexlify(icmpHeader)))
+        # print('packetData: {0}'.format(binascii.hexlify(packetData)))
+        # print('icmpType: {0}, expected: {1}'.format(icmpType, ICMP_ECHO_REPLY_TYPE))
+        # print('icmpCode: {0}, expected: {1}'.format(icmpCode, ICMP_ECHO_REPLY_CODE))
+        # print('packetID: {0}, expected: {1}'.format(packetID, ID))
+        # print('packetSequence: {0}, expected: {1}'.format(packetSequence, 1))
+        # print('icmpChecksum: {0}, expected: {1}'.format(icmpChecksum, expectedIcmpChecksum))
 
         # Check the received packet comes from the expected host, it has the correct ICMP type and code, check the
         # packet is not corrupted, and check this is the packet we are expecting by matching the ID and sequence numbers
         if addr[0] == destAddr and icmpType == ICMP_ECHO_REPLY_TYPE and icmpCode == ICMP_ECHO_REPLY_CODE and \
                 packetID == ID and packetSequence == 1 and icmpChecksum == expectedIcmpChecksum:
-            return timeReceived - timeSent
+            # Return the RTT in ms
+            rtt = (timeReceived - timeSent) * 1000.0
+            return '{0}: ICMP seq={1} TTL={2} RTT={3:.3f}ms'.format(addr[0], packetSequence, ipTTL, rtt), rtt
         elif icmpType == ICMP_ERROR_TYPE and icmpCode == ICMP_DEST_NET_UNREACHABLE_CODE and \
-                packetID == ID and packetSequence == 1 and icmpChecksum == expectedIcmpChecksum:
-            return 'Destination network unreachable.'
+                icmpChecksum == expectedIcmpChecksum:
+            return 'Destination network unreachable.', 'na'
         elif icmpType == ICMP_ERROR_TYPE and icmpCode == ICMP_DEST_HOST_UNREACHABLE_CODE and \
-                packetID == ID and packetSequence == 1 and icmpChecksum == expectedIcmpChecksum:
-            return 'Destination host unreachable.'
+                icmpChecksum == expectedIcmpChecksum:
+            return 'Destination host unreachable.', 'na'
         elif icmpType == ICMP_ERROR_TYPE and icmpCode == ICMP_DEST_PROTO_UNREACHABLE_CODE and \
-                packetID == ID and packetSequence == 1 and icmpChecksum == expectedIcmpChecksum:
-            return 'Destination protocol unreachable.'
+                icmpChecksum == expectedIcmpChecksum:
+            return 'Destination protocol unreachable.', 'na'
         elif icmpType == ICMP_ERROR_TYPE and icmpCode == ICMP_DEST_PORT_UNREACHABLE_CODE and \
-                packetID == ID and packetSequence == 1 and icmpChecksum == expectedIcmpChecksum:
-            return 'Destination port unreachable.'
+                icmpChecksum == expectedIcmpChecksum:
+            return 'Destination port unreachable.', 'na'
         elif icmpType == ICMP_ERROR_TYPE and icmpCode == ICMP_DEST_NET_UNKNOWN_CODE and \
-                packetID == ID and packetSequence == 1 and icmpChecksum == expectedIcmpChecksum:
-            return 'Destination network unknown.'
+                icmpChecksum == expectedIcmpChecksum:
+            return 'Destination network unknown.', 'na'
         elif icmpType == ICMP_ERROR_TYPE and icmpCode == ICMP_DEST_HOST_UNKNOWN_CODE and \
-                packetID == ID and packetSequence == 1 and icmpChecksum == expectedIcmpChecksum:
-            return 'Destination host unknown.'
+                icmpChecksum == expectedIcmpChecksum:
+            return 'Destination host unknown.', 'na'
 
         # Fill in end
         timeLeft = timeLeft - howLongInSelect
         if timeLeft <= 0:
-            return "Request timed out."
+            return 'Request timed out.', 'na'
+
 
 def sendOnePing(mySocket, destAddr, ID):
     # Header is type (8), code (8), checksum (16), id (16), sequence (16)
@@ -171,9 +183,10 @@ def sendOnePing(mySocket, destAddr, ID):
     header = struct.pack("bbHHh", ICMP_ECHO_REQUEST, 0, myChecksum, ID, 1)
     packet = header + data
 
-    mySocket.sendto(packet, (destAddr, 1)) # AF_INET address must be tuple, not str
+    mySocket.sendto(packet, (destAddr, 1))  # AF_INET address must be tuple, not str
     # Both LISTS and TUPLES consist of a number of objects
     # which can be referenced by their position number within the object.
+
 
 def doOnePing(destAddr, timeout):
     icmp = getprotobyname("icmp")
@@ -189,6 +202,7 @@ def doOnePing(destAddr, timeout):
     mySocket.close()
     return delay
 
+
 def ping(host, timeout=1):
     # timeout=1 means: If one second goes by without a reply from the server,
     # the client assumes that either the client's ping or the server's pong is lost
@@ -198,13 +212,21 @@ def ping(host, timeout=1):
     # Send ping requests to a server separated by approximately one second
     delayList = list()
     try:
-        while 1 :
+        while 1:
             delay = doOnePing(dest, timeout)
-            print(delay)
-            delayList.append(delay)
-            time.sleep(1)# one second
+            print(delay[0])
+            delayList.append(delay[1])
+            time.sleep(1)  # one second
+    except error as e:
+        # Handle socket errors such as network unreachable
+        delay = e[1]
+        print(delay)
     except KeyboardInterrupt:
         calcStats(delayList)
+        delay = 'Keyboard interrupt.'
+
     return delay
 
-#ping("google.com")
+
+if __name__ == '__main__':
+    ping("www.google.com")
--- README.md
+++ README.md
@@ -7,10 +7,29 @@ ICMP pinger application.
 
 **Contributions**: Based on the source code provided by Ramesh Yerraballi.
 
->Copyright :copyright: 2019 of Jose Carlos Martinez Garcia-Vaso.
+> Copyright :copyright: 2019 of Jose Carlos Martinez Garcia-Vaso.
 
 
 Usage
 -----
 
-**WIP**
+Run the Python2 interpreter with admin privileges, then:
+
+```python
+import ICMPPinger
+ICMPPinger.ping("<URL or IP>", timeout=<time in sec>)
+```
+
+The `timeout` parameter is optional, and it will default to 1sec if it is not specified. You can also run a one-liner 
+on the shell:
+
+```bash
+sudo python -c 'import ICMPPinger; ICMPPinger.ping("<URL or IP>", timeout=<time in sec>)'
+```
+
+Running the script from the shell as shown below will ping the test case `www.google.com` with a default timeout of 
+1sec:
+
+```bash
+sudo python ICMPPinger.py
+```
